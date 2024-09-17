9babec2fbd81fb8dfda8546fea9f9ba3110ceda0
Gutoy2k <gustavo.luiz.oliveira@escola.pr.gov.br>
2024-09-17 11:33:37 -0300

    a

--- app.js
+++ app.js
@@ -3,5 +3,10 @@ let container = document.getElementById('container');
 let cartao = document.createElement('article');
 cartao.className- 'cartao';
 cartao.innerHTML = `
-`
+ <div class="cartao_conteudo">
+            <H3>${categoria}</H3>
+            <div class="cartao_conteudo_pergunta">
+                <p>${pergunta}</p>
+            </div>
+        </div>`
 }
