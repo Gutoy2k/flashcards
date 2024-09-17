abc09d0d27f930b3b282d7342937470f2b35c70a
Gutoy2k <gustavo.luiz.oliveira@escola.pr.gov.br>
2024-09-17 11:16:53 -0300

    a

---
+++ perguntas.js
@@ -0,0 +1,5 @@
+criaCartao(
+    'ed. financeira',
+    'o que é investir?',
+    '​Investir é basicamente emprestar o seu dinheiro ao banco para receber os juros pelo dinheiro emprestado Ou ainda aplicar o seu dinheiro em um negócio que vai render lucros',
+)
